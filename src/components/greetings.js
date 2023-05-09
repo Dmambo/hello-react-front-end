0ed8676732e09adc9ffc69e0104787381a0a4054
Dmambo <mambotrader0@gmail.com>
2023-05-09 20:13:40 +0530

    Added the linter files and update the readme

--- src/components/greetings.js
+++ src/components/greetings.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Greetings = (props) => {
-  return (
-    <div>
-      <h1>All Greeetings</h1>
-      {props.greetings.map((item) => {
-        return <p key={item.id}>{item.message}</p>;
-      })}
-    </div>
-  );
+const Greetings = ({ greetings }) => (
+  <div>
+    <h1>All Greeetings</h1>
+    {greetings.map((item) => (
+      <p key={item.id}>{item.message}</p>
+    ))}
+  </div>
+);
+
+Greetings.propTypes = {
+  greetings: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      message: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
 };
 
 export default Greetings;
